dd7044092ff7e8d8678a25cdaac77d2afa78d9cc
Shubhang <s.saraogi1902@gmail.com>
2023-08-09 14:09:56 +0530

    cors fixed

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors())
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(compression()); 
 app.use(
   helmet.contentSecurityPolicy({

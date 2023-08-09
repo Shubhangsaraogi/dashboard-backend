bd265582161fa1eb3af4220951a13d1ca8487c3c
Shubhang <s.saraogi1902@gmail.com>
2023-08-09 18:30:48 +0530

    rm header from index

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const port = 5000;
 
 app.use(cors())
 app.use(express.json());
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 app.use('/api/storedata',require('./router/storedata'));
 app.use('/api/',require('./router/auth'));
 app.use('/api/question',require('./router/question'));

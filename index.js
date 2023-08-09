8d1dec1c53d16802bf0d4619548283e434ca58d1
Shubhang <s.saraogi1902@gmail.com>
2023-08-09 23:54:22 +0530

    home route added

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const port = 5000;
 
 app.use(cors({ origin:'https://notes-app.000.pe/' , credentials :  true}));
 app.use(express.json());
-
-app.use('/api/storedata',require('./router/storedata'));
+app.use('/',(req,res)=>{
+  res.json({"message":"hello there this is notes backend server"});
+})
 app.use('/api/',require('./router/auth'));
 app.use('/api/question',require('./router/question'));
+app.use('/api/storedata',require('./router/storedata'));
 app.use('/api/submission',require('./router/compiler'));
 
 app.listen(port, () => {

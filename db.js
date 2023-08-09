fc3d37746dfef38bdf331ef119bd7df4e264ff4b
Shubhang <s.saraogi1902@gmail.com>
2023-08-09 23:25:44 +0530

    MONGODB_URL env variable added

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGO_URL;
+const mongoURI = process.env.MONGODB_URL;
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)

338e300c622f9734bd6c18481c4bd408e40c9c90
Shubhang <s.saraogi1902@gmail.com>
2023-08-10 00:29:50 +0530

    MONGODB_URI

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGO_URL;
+const mongoURI = process.env.MONGODB_URI;
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)

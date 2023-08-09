3858651029409e704634eab740d333f84be944ed
Shubhang <s.saraogi1902@gmail.com>
2023-08-09 18:45:37 +0530

    header in index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000' , credentials :  true}));
 app.use(express.json());
 
 app.use('/api/storedata',require('./router/storedata'));

136d1e912ad7537542912d381f1feae9f1233ff9
Shubhang <s.saraogi1902@gmail.com>
2023-08-23 00:53:27 +0530

    cors at last of routes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors({ origin:'https://notes-app.000.pe/' , credentials :  true}));
+// app.use(cors({ origin:'https://notes-app.000.pe/' , credentials :  true}));
 app.use(express.json());
 // app.use('/',(req,res)=>{
 //   res.json({"message":"hello there this is notes backend server"});
@@ -15,7 +15,7 @@ app.use('/api/',require('./router/auth'));
 app.use('/api/question',require('./router/question'));
 app.use('/api/storedata',require('./router/storedata'));
 app.use('/api/submission',require('./router/compiler'));
-
+app.use(cors());
 app.listen(port, () => {
   console.log(`Code Arena backend listening on port ${port}`)
 })

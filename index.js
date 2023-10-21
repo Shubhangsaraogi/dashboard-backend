c95d2586e9ac7a773b7a016f7df8b70db020dc6f
Shubhang <s.saraogi1902@gmail.com>
2023-10-22 03:13:14 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors({origin:'https://notes-mate.vercel.app'}));
+app.use(cors({origin:'http://localhost:3000'}));
 app.use(express.json());
 // app.use('/',(req,res)=>{
 //   res.json({"message":"hello there this is notes backend server"});
 // })
 app.use('/api/',require('./router/auth'));
-app.use('/api/question',require('./router/question'));
-app.use('/api/storedata',require('./router/storedata'));
-app.use('/api/submission',require('./router/compiler'));
+
 app.listen(port, () => {
   console.log(`Code Arena backend listening on port ${port}`)
 })
--- router/auth.js
+++ router/auth.js
@@ -3,13 +3,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
 const fetch_id = require('../middleware/fetch_id');
+const Profile = require('../models/profile');
 // route 1 to signup the user
-router.post('/createuser', [
-  body('name', 'Enter a valid String').isLength({ min: 3 }),
+router.post('/user', [
+  body('name', 'Enter a valid String').exists(),
   body('email', 'Enter a valid email').isEmail(),
-  body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
 
   let success = false;
@@ -20,91 +19,86 @@ router.post('/createuser', [
   try {
     let user = await User.findOne({ email: req.body.email });
     if (user) {
-      return res.status(400).json({ success, error: "User already exist with this email" });
+      const data = {
+        id: user.id
+      }
+      var authToken = jwt.sign(data, process.env.JWT_SECRET);
+      success = true;
+      return res.status(200).json({ success, email: req.body.email, authToken });
     }
     // console.log(user);
-    const salt = await bcrypt.genSalt(10);
-    secPass = await bcrypt.hash(req.body.password, salt)
+    // const salt = await bcrypt.genSalt(10);
+    // secPass = await bcrypt.hash(req.body.password, salt)
       user = await User.create({
         name: req.body.name,
-        password: secPass,
         email: req.body.email,
-        role: "admin"
       });
-    // res.json(user);
     const data = {
       id: user.id
     }
     var authToken = jwt.sign(data, process.env.JWT_SECRET);
     success = true;
-    res.status(200).json({ success, email: req.body.email, authToken });
+    return res.status(200).json({ success, email: req.body.email, authToken });
+    
   }
   catch (error) {
     console.error(error.message);
-    res.status(500).send(success, "Internal Server Error");
+    return res.json(200,success, "Internal Server Error");
   }
 
 })
+router.get('/getprofile', fetch_id, async (req, res) => {
+  let success =false;
+  try {
+    const userId = req.user;
+    let profile = await Profile.findById(userId);
+    if(profile)
+    {
+      success=true;
+      return res.status(200).json({success,profile} );
+    }
+    else
+      return res.status(200).json("No profile added" );
 
-// route 2 to login the user
-router.post('/login', [
-  body('email', 'Enter a valid email').isEmail(),
-  body('password', 'Password cannot be empty').exists()
-], async (req, res) => {
-
-  let success = false;
+  }
+  catch (error) {
+    console.error(error.message);
+    return res.status(500).send("No profile created");
+  }
+})
+router.post('/addprofile', fetch_id,  async (req, res) => {
+  let success=false;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ success, errors: errors.array() });
+    console.log(errors);
+    return res.status(400).json({ errors: errors.array() });
   }
-
-  const {email,password}=req.body;
   try {
-    let user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ success, error: "Please Login with correct credentials" });
+    const userId = req.user;
+    console.log(userId);
+    let profile = await Profile.findById(userId);
+    if (!profile) {
+      success=true;
+      const { email,name,phone, instagram, youtube } = req.body;
+      console.log("data received");
+      console.log(req.body.email,req.body.name,req.body.phone,req.body.instagram,req.body.youtube);
+        const profile = await new Profile({ email,name,phone,instagram,youtube, _id: userId }).save();
+        return res.status(200).json({success,profile});
     }
-    // console.log(user);
-    const passwordCompare =await bcrypt.compare(password, user.password);
-
-    if (!passwordCompare){
-      return res.status(400).json({ success, error: "Please Login with correct credentials" });
+    else
+    {
+      success=true;
+      return res.status(200).json({success,profile} );
     }
-
-    const data = {
-      id: user.id
-    }
-    var authToken = jwt.sign(data, process.env.JWT_SECRET);
-    success = true;
-    res.status(200).json({ success, email: req.body.email, authToken });
-
+    console.log(profile);
   }
   catch (error) {
     console.error(error.message);
-    res.status(500).send(success, "Internal Server Error");
+    return res.status(500).send("Intenal Server Error");
   }
 
 })
 
-// Route 3 to fetch the user
-router.post('/getuser', fetch_id, async (req, res) => {
 
-  let success = false;
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ success, errors: errors.array() });
-  }
-  try {
-    userId = req.user;
-    let user = await User.findById(userId).select("-password");
-    success = true;
-    res.send({ success, user });
-  }
-  catch (error) {
-    console.error(error.message);
-    res.status(500).send(success, "Intenal Server Error");
-  }
-
-})
 
 module.exports = router;
